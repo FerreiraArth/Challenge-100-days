c64eebb74254df8cf6b20550dcecc0d96bd319ae
FerreiraArth <arthurferreir4a4@gmail.com>
2023-10-04 18:03:50 -0300

    conclued "C" button

--- 01-100 - CALC/script.js
+++ 01-100 - CALC/script.js
@@ -2,3 +2,7 @@ function insert(num){
     var numero = document.getElementById('res').innerHTML;
     document.getElementById('res').innerHTML = numero + num;
 }
+
+function clean(){
+    document.getElementById('res').innerHTML = "";
+}
