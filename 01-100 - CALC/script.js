320852811987d39f96846b4abe366cb5e90481c7
FerreiraArth <arthurferreir4a4@gmail.com>
2023-10-04 18:51:48 -0300

    conclued

--- 01-100 - CALC/script.js
+++ 01-100 - CALC/script.js
@@ -6,3 +6,15 @@ function insert(num){
 function clean(){
     document.getElementById('res').innerHTML = "";
 }
+
+function back(){
+    var res = document.getElementById('res').innerHTML;
+    document.getElementById('res').innerHTML = res.substring(0, res.length -1)
+}
+
+function calc(){
+    var res = document.getElementById('res').innerHTML;
+    if(res){
+        document.getElementById('res').innerHTML = eval(res)
+    }
+}
