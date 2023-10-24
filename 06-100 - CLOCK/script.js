558fe31d2fea7625e74d77f97e04f992da9590f8
FerreiraArth <arthurferreir4a4@gmail.com>
2023-10-23 21:03:26 -0300

    favicon

--- 06-100 - CLOCK/script.js
+++ 06-100 - CLOCK/script.js
@@ -9,7 +9,7 @@ const clock = setInterval (function time() {
   let s = dateToday.getSeconds();
 
   if(hr < 10) hr = '0' + hr;
-  if(min < 10) min = '0' + misn;
+  if(min < 10) min = '0' + min;
   if(s < 10) s = '0' + s;
 
   hours.textContent = hr;
